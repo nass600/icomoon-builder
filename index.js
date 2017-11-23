fe386eb2a41b3982b05c84e506706db1127ce860
Ignacio Velazquez <ivelazquez85@gmail.com>
2017-11-23 22:58:29 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const program = require('commander');
+// Require logic.js file and extract controller functions using JS destructuring assignment
+const build = require('./cli');
+const package = require('./package.json');
+
+program
+  .version(package.version)
+  .usage('[options] <file ...>')
+  .description('Update your icon library from icomoon zip file');
+
+program
+  .command('build <icomoonZipFile>')
+  .alias('b')
+  .description('Build icomoon')
+  .action((icomoonZipFile) => {
+    build(icomoonZipFile);
+  });
+
+program.parse(process.argv);
+
+if (program.args.length === 0) program.help();
